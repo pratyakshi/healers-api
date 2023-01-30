61ed69b17c2deb418cc12f08ce021916c67fa5d6
orv3 <ratya.g@protonmail.com>
2023-01-30 21:30:21 +0530

    Redefined data

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const express = require('express')
-const cors = require("cors")
-const app = express()
+const express = require('express');
+const app = express ();
+const port = process.env.PORT || 3000;
 
+const apiData = require("./data.json");
 
-const port = process.env.PORT || 3000
-
-const apiData = require("./data.json")
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/ healers", (req,res) => {
+  res.send(apiData);
 });
 
 app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`)
-  });
+  console.log(`Listening on ${port}`);
+});
